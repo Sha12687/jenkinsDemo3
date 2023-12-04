e5830899886bbe1dec2560d768bcc9e5319da005
Shashant <shashantchaudhary999@gmail.com>
2023-12-04 15:48:35 +0530

    wer

---
+++ XUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace XUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
